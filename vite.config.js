064af2d1d7535a6fff8cb74cbca10fca6244300f
BartonBowell <carson@carsonppowell.com>
2025-07-17 21:21:16 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/choggers.com/', // This matches your GitHub Pages repository name
+  base: '/', // This matches your GitHub Pages repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
